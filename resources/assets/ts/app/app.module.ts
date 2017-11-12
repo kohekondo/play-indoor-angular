099f2be4eede1dd23bfd6e91d24cf3532c581235
kohekondo <1211lta@gmail.com>
2017-11-12 16:14:35 +0900

    angular ＆ clarity環境構築終了

--- resources/assets/ts/app/app.module.ts
+++ resources/assets/ts/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
+import { ClarityModule } from 'clarity-angular';
 import { AppComponent } from './app.component';
 @NgModule({
     imports: [
-        BrowserModule
+        BrowserModule,
+        ClarityModule.forRoot()
     ],
     declarations: [
         AppComponent
